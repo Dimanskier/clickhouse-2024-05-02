211b3c29a82bef6007214af993ff341eea32e54c
a.polyakov <i@a-polyakov.ru>
2024-05-28 12:02:27 +0300

    add cloud dpeloyment

---
+++ cloud/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+
+}
+
+resource "yandex_vpc_network" "default" {}
+
+resource "yandex_vpc_subnet" "foo" {
+  network_id     = yandex_vpc_network.default.id
+  v4_cidr_blocks = ["10.5.0.0/24"]
+  zone = "ru-central1-a"
+}
+
+resource "yandex_mdb_clickhouse_cluster" "clickhouse-dev" {
+  environment = "PRESTABLE"
+  name        = "clickhouse-dev"
+  network_id  = yandex_vpc_network.default.id
+
+  version = "23.3"
+  admin_password = var.clickhouse_password
+  sql_user_management = true
+  sql_database_management = true
+
+  clickhouse {
+    resources {
+      resource_preset_id = "s3-c2-m8"
+      disk_type_id = "network-hdd"
+      disk_size = 120
+    }
+
+    config {
+      log_level = "TRACE"
+      max_concurrent_queries = 100
+      max_connections = 100
+    }
+  }
+
+  host {
+    type = "CLICKHOUSE"
+    zone = "ru-central1-a"
+    assign_public_ip = true
+    subnet_id = yandex_vpc_subnet.foo.id
+  }
+
+  cloud_storage {
+    enabled = false
+  }
+
+  maintenance_window {
+    type = "ANYTIME"
+  }
+}
---
+++ cloud/variables.tf
@@ -0,0 +1,5 @@
+variable "clickhouse_password" {
+  description = "Clickhouse admin password"
+  type = string
+  sensitive = true
+}
